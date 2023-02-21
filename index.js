5d5d094ed32726ccea48478389a70e97d952493d
Catzilla <11861208+Catzilla@users.noreply.github.com>
2023-02-21 14:02:10 +0200

    Fixed class name

--- index.js
+++ index.js
@@ -3,7 +3,7 @@ const jp = require('../../../../current/node_modules/jsonpath')
 const Base = require('../../../../current/core/server/adapters/sso/Base')
 const User = require('../../../../current/core/server/models/user').User
 
-module.exports = class OryKratosHeaderSSOAdapter extends Base {
+module.exports = class HeaderSSOAdapter extends Base {
     constructor(config) {
         super()
 
